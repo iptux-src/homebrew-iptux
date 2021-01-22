a1a46649cf4582aa9bad7b174d631a666f3129e1
LI Daobing <lidaobing@gmail.com>
2021-01-22 20:11:36 +0800

    Create iptux@head.rb

---
+++ Formula/iptux@head.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class IptuxAtHead < Formula
+  head 'https://github.com/iptux-src/iptux.git'
+  homepage 'https://github.com/iptux-src/iptux'
+  url 'https://github.com/iptux-src/iptux/archive/v0.7.5.tar.gz'
+  sha256 '37fd2618e888d44b3ddcc21e2d497f0a8dcbdb2adcb23fd137fb8e56d2d46919'
+
+  depends_on 'gettext'
+  depends_on 'jsoncpp'
+  depends_on 'gstreamer' => :optional
+  depends_on 'gst-plugins-base' => ["with-ogg", "with-libvorbis"] if build.with? "gstreamer"
+  depends_on 'gst-plugins-good' if build.with? "gstreamer"
+  depends_on 'pkg-config' => :build
+  depends_on 'gtk+3'
+  depends_on 'glog'
+  depends_on 'meson' => :build
+  depends_on 'ninja' => :build
+
+  def install
+    mkdir "build" do
+      system "/usr/local/opt/meson/bin/meson", *std_meson_args, "-Dwith-vala=false", ".."
+      system "/usr/local/opt/ninja/bin/ninja", "-v"
+      system "/usr/local/opt/ninja/bin/ninja", "install", "-v"
+    end
+  end
+end
